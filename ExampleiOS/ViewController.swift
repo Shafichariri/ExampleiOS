20863ed08f4f990ef51d13e3ed4e71297e1bc6b6
Shafic Hariri <chafic.elhariri@neo.ae>
2020-04-18 23:48:53 +0300

    Force crash and change background

--- ExampleiOS/ViewController.swift
+++ ExampleiOS/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+           // MSCrashes.generateTestCrash()
+            self.view.backgroundColor = .red
+        }
     }
 
 
