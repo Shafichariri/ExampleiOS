78d7398eba29b94519d3773813c7179d944e0f1a
Shafic Hariri <chafic.elhariri@neo.ae>
2020-04-18 10:54:19 +0300

    First commit

---
+++ ExampleiOSTests/ExampleiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExampleiOSTests.swift
+//  ExampleiOSTests
+//
+//  Created by Shafic Hariri on 4/18/20.
+//  Copyright © 2020 Shafic Hariri. All rights reserved.
+//
+
+import XCTest
+@testable import ExampleiOS
+
+class ExampleiOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
